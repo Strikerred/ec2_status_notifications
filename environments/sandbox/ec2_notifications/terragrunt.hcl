8e6f9f02c95f64d8ea9fa4dc34c1cee1c3d03ec9
gomez_fausto@hotmail.com <fausto.gomez@clir.eco>
2022-10-16 15:41:49 -0700

    CON-157 initialized variables.tf & terragrunt.hcl

--- environments/sandbox/ec2_notifications/terragrunt.hcl
+++ environments/sandbox/ec2_notifications/terragrunt.hcl
@@ -1,21 +1,21 @@
 
-    # sandbox/ec2_notifications/terragrunt.hcl
-    locals{
-        repo_path = run_cmd("git", "rev-parse","--show-toplevel")
-    }
+# sandbox/ec2_notifications/terragrunt.hcl
+locals {
+  repo_path = run_cmd("git", "rev-parse", "--show-toplevel")
+}
 
-    include "root" {
-        path = find_in_parent_folders()
-    }
-    
-    terraform {
-        source = "${local.repo_path}/services//ec2_notifications"
-    }
-    
-    inputs = {
-        environment = "sandbox"
-        aws_region = "us-west-2"
-        service_name = "ec2_notifications"
-        department = "tech delivery"
-    }
+include "root" {
+  path = find_in_parent_folders()
+}
+
+terraform {
+  source = "${local.repo_path}/services//ec2_notifications"
+}
+
+inputs = {
+  environment    = "sandbox"
+  aws_region     = "us-west-2"
+  service_name   = "ec2_notifications"
+  department     = "tech delivery"
+}
     
--- services/ec2_notifications/variables.tf
+++ services/ec2_notifications/variables.tf
@@ -1,21 +1,20 @@
 
-    variable "aws_region" {
-        description = "AWS Region in which this service will be deployed"
-        type        = string
-    }
+variable "aws_region" {
+  description = "AWS Region in which this service will be deployed"
+  type        = string
+}
 
-    variable "environment" {
-        description = "Environment for which this service will be deployed"
-        type        = string
-    }
+variable "environment" {
+  description = "Environment for which this service will be deployed"
+  type        = string
+}
 
-    variable "department" {
-        description = "Department responsible for this service"
-        type        = string
-    }
+variable "department" {
+  description = "Department responsible for this service"
+  type        = string
+}
 
-    variable "service_name" {
-        description = "Name of the service"
-        type        = string
-    }
-    
+variable "service_name" {
+  description = "Name of the service"
+  type        = string
+}
