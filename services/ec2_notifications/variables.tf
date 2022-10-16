d6f5e7364c94fe409cbfd3536e6c30d04465d846
gomez_fausto@hotmail.com <fausto.gomez@clir.eco>
2022-10-16 15:15:39 -0700

    CON-157 initial commit

---
+++ services/ec2_notifications/variables.tf
@@ -0,0 +1,21 @@
+
+    variable "aws_region" {
+        description = "AWS Region in which this service will be deployed"
+        type        = string
+    }
+
+    variable "environment" {
+        description = "Environment for which this service will be deployed"
+        type        = string
+    }
+
+    variable "department" {
+        description = "Department responsible for this service"
+        type        = string
+    }
+
+    variable "service_name" {
+        description = "Name of the service"
+        type        = string
+    }
+    
