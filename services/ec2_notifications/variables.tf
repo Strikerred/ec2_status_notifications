0abd870e74bd852066aa42f0881c7837d30f7879
Fausto Gomez <48969442+Strikerred@users.noreply.github.com>
2022-12-01 15:34:02 -0800

    Added ids' variables.

--- services/ec2_notifications/variables.tf
+++ services/ec2_notifications/variables.tf
@@ -18,3 +18,28 @@ variable "service_name" {
   description = "Name of the service"
   type        = string
 }
+
+variable "instance_id" {
+  description = "Ec2 Instance ID"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "security group ID"
+  type        = list(string)
+}
+
+variable "eni_attachment_id" {
+  description = "ID that is generated once the eni is attached to a resource"
+  type        = list(string)
+}
+
+variable "network_interface_id" {
+  description = "Network interface ID"
+  type        = list(string)
+}
+
+variable "eip_association_id" {
+  description = "Elastic IP association ID"
+  type        = list(string)
+}
